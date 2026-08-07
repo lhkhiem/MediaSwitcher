58877a2430273e0fef4bb318e0f163de886920c3
developer-pressup <developer-pressup@example.com>
2026-08-07 21:24:09 +0700

    feat: add windeployqt step to build script for automatic dependency deployment

--- build.bat
+++ build.bat
@@ -2,3 +2,4 @@
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 cmake -S . -B build -DCMAKE_PREFIX_PATH="C:\Qt\6.11.1\msvc2022_64"
 cmake --build build
+C:\Qt\6.11.1\msvc2022_64\bin\windeployqt.exe build\bin\Debug\MediaSwitcher.exe
